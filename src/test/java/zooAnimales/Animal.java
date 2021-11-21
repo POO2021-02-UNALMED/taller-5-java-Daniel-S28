a2eb30f0fdfc2877f054155dfe1eccd82872eef5
Daniel-S28 <80227692+Daniel-S28@users.noreply.github.com>
2021-11-20 21:17:48 -0500

    cambio en string y totalportipo

--- src/test/java/zooAnimales/Animal.java
+++ src/test/java/zooAnimales/Animal.java
@@ -29,13 +29,17 @@ public class Animal {
 			   "Aves:"+ Ave.cantidadAves() + "\n" +
 			   "Reptiles:"+ Reptil.cantidadReptiles() + "\n" +
 			   "Peces:"+Pez.cantidadPeces() + "\n" +
-			   "Anfibios:"+Anfibio.cantidadAnfibios() + "\n";
+			   "Anfibios:"+Anfibio.cantidadAnfibios();
 	}
 	
 	public String toString() {
+		if (zona!=null){
+			return "Mi nombre es " + this.nombre + " tengo una edad de" + this.edad + ", habito en" + 
+					this.habitat + " y mi genero es" + this.genero + 
+					", la zona en la que me ubico es" + zona + ", en el" + zona.getZoo();
+		}
 		return "Mi nombre es " + this.nombre + " tengo una edad de" + this.edad + ", habito en" + 
-				this.habitat + " y mi genero es" + this.genero + 
-				", la zona en la que me ubico es" + zona + ", en el" + zona.getZoo();
+				this.habitat + " y mi genero es" + this.genero;
 	}
 	
 	public String movimiento() {

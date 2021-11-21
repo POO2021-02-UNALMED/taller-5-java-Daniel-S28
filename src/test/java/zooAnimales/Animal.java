4f0f8a8f7cb429c2fbeec16f8d7e307297bee50d
Daniel-S28 <80227692+Daniel-S28@users.noreply.github.com>
2021-11-20 21:31:07 -0500

    corregi mas benditos espacios

--- src/test/java/zooAnimales/Animal.java
+++ src/test/java/zooAnimales/Animal.java
@@ -34,12 +34,12 @@ public class Animal {
 	
 	public String toString() {
 		if (zona!=null){
-			return "Mi nombre es " + this.nombre + ", tengo una edad de" + this.edad + ", habito en" + 
-					this.habitat + " y mi genero es" + this.genero + 
-					", la zona en la que me ubico es" + zona + ", en el" + zona.getZoo();
+			return "Mi nombre es " + this.nombre + ", tengo una edad de " + this.edad + ", habito en " + 
+					this.habitat + " y mi genero es " + this.genero + 
+					", la zona en la que me ubico es " + zona + ", en el " + zona.getZoo();
 		}
-		return "Mi nombre es " + this.nombre + ", tengo una edad de" + this.edad + ", habito en" + 
-				this.habitat + " y mi genero es" + this.genero;
+		return "Mi nombre es " + this.nombre + ", tengo una edad de " + this.edad + ", habito en " + 
+				this.habitat + " y mi genero es " + this.genero;
 	}
 	
 	public String movimiento() {
